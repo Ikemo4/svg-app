3dd0a7f4377064bec5a926fc5a245230b1547be0
Ikemo4 <Isaac.morris@flybreeze.com>
2022-10-20 11:12:18 -0600

    messin with svgs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
+import plainMultiple from './plain-multiple.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div
+          style={{
+            height: '200px',
+            width: '200px',
+            backgroundColor: 'transparent',
+            backgroundRepeat: 'no-repeat',
+            alignItems: 'center',
+            backgroundImage: `url(${plainMultiple})`
+          }}
+        >Boop</div>
       </header>
     </div>
   );
